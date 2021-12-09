c871d38e5fd11324185a63b0906f903fe3787f95
vera <vera@LAPTOP-TSLO5PC0>
2021-12-09 14:26:10 +0100

    Dodaj pliki projektów.

---
+++ udemyOBpolimorfizm2/udemyOBpolimorfizm2.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Postac {
+public:
+	virtual void PrzedstawSie() {
+		cout << "Metoda Postac" << endl;
+	}
+};
+
+class Czlowiek : public Postac {
+public:
+	void PrzedstawSie() {
+		cout << "Metoda Czlowiek" << endl;
+	}
+};
+
+class Mnich : public Postac {
+public:
+	void PrzedstawSie() {
+		cout << "Metoda Mnich" << endl;
+	}
+};
+
+void KtoTo(Postac& obiekt) {
+	obiekt.PrzedstawSie();
+}
+
+	int main() {
+
+		Czlowiek obiekt1;
+		Mnich obiekt2;
+
+		KtoTo(obiekt1);
+		KtoTo(obiekt2);
+
+};
